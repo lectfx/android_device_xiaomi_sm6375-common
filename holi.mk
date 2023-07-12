da4cacfbb27307da9fc4db8d23d2c452e29732d3
nnippon99 <adamayyad1999@hotmail.com>
2023-07-12 14:26:05 +0200

    sm6375-common: switch to prebuilt libdisplayconfig.qti.so

--- holi.mk
+++ holi.mk
@@ -118,7 +118,6 @@ PRODUCT_PACKAGES += \
     memtrack.default
 
 PRODUCT_PACKAGES += \
-    libdisplayconfig.qti \
     libdisplayconfig.system.qti \
     libqdMetaData \
     libqdMetaData.system \
